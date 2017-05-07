ac1a96ab4fc98f2a9d355e045c91feec495f3c50
Speznar <Spencer.G.Phillips@gmail.com>
2017-05-07 16:05:28 -0700

    Javasript Addition
    Implemented function to keep track of the pieces that the user has
    added.  Inventory is stored in a list.

--- pages/admin-dashboard/Monopoly Game.js
+++ pages/admin-dashboard/Monopoly Game.js
@@ -1,3 +1,6 @@
+// This is simple data entry.  We got the names of the prizes from the gameboard and made lists of the pieces that allow you to win those prizes...
+// FOR EXAMPLE: You win the $5 Cash prize by collecting the tickets with code numbers "9J36A","9J37B","9J38C", and "9J39D"
+//		therefore we initialized a list variable and filled it with the game piece codes that will make you win the prize.
 var prizeCheckList$5Cash = ["9J36A","9J37B","9J38C","9J39D"];
 var prizeCheckList$5GroceryGiftCard = ["9H32A","9H33B","9H34C","9H35D"];
 var prizeCheckList$10Cash = ["9G28A","9G29B","9G30C","9G31D"];
@@ -15,6 +18,7 @@ var prizeCheckList$200FamilyPicnic = ["8Q41A","8Q42B","8Q43C","8Q44D"];
 var prizeCheckList$300SmartWatch = ["8P45A","8P46B","8P47C","8P48D"];
 var prizeCheckList$500GroceryGiftCard = ["8N49A","8N50B","8N51C","8N52D"];
 var prizeCheckList$1000LaptopComputer = ["8M53A","8M54B","8M55C","8M56D"];
+var prizeCheckList$1000GroceryGiftCard = ["8L57A","8L58B","8L59C","8L60D"]
 var prizeCheckList$1000Cash = ["8K61A","8K62B","8K63C","8K64D"];
 var prizeCheckList$1500LEDHDTV = ["8J65A","8J66B","8J67C","8J68D"];
 var prizeCheckList$1500GasGrillAndGroceries = ["8H69A","8H70B","8H71C","8H72D"];
@@ -28,3 +32,54 @@ var prizeCheckList$40000HomeMakeover = ["8D86A","8D87B","8D88C","8D89D","8D90E"]
 var prizeCheckList$100000CashOrLuxuryCar = ["8E81A","8E82B","8E83C","8E84D","8E85E"];
 var prizeCheckList$1000000Cash = ["8Z01A","8Z02B","8ZO3C","8Z04D","8Z05E","8Z06F","8Z07G","8Z08H"];
 var prizeCheckList$1000000VacationHome = ["8Y09A","8Y10B","8Y11C","8Y12D","8Y13E","8Y14F","8Y15G","8Y16H"];
+
+//  This following lines of code dump all of the previous lists into one list.
+var completePieceCodeList = [];
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$5Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$5GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$10Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$10GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$15GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$25FandangoGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$25Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$25GiftCardMall);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$25GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$50GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$100Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$100GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$200Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$200FamilyPicnic);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$300SmartWatch);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$500GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$1000LaptopComputer);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$1000GroceryGiftCard);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$1000Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$1500LEDHDTV);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$1500GasGrillAndGroceries);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$5000Groceries);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$5000Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$10000FamilyVacation);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$10000FourWheeler);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$20000CollegeTuition);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$35000VehicleOfYourChoice);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$40000HomeMakeover);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$100000CashOrLuxuryCar);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$1000000Cash);
+completePieceCodeList = completePieceCodeList.concat(prizeCheckList$1000000VacationHome);
+
+var inventoryOfPiecesByIndex = [];
+for (var i=0; i<138; i++){
+	inventoryOfPiecesByIndex.push(0);
+}
+
+function addGamePiece(pieceCode){
+	
+	var inputCodeIndex = completePieceCodeList.indexOf(pieceCode);
+	
+	if (inputCodeIndex != -1)/*<-- tests if the piece code is valid*/{
+		inventoryOfPiecesByIndex[inputCodeIndex] += 1; //<-- modifies inventory of valid tickets.
+	}
+	else {
+		alert("The code you entered is not a valid code.")
+	}
+}
